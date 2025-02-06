f0f368111e0f65c732ee3803edd68213d58a8400
lyumii <dmly1124@gmail.com>
2025-02-06 10:00:24 +0100

    hover over items

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,14 @@
+import React, { useState } from "react";
+
 export default function Card(props) {
+  const [isActive, setIsActive] = useState(false);
+
   return (
-    <article>
+    <article
+      onMouseOver={() => setIsActive(true)}
+      onMouseLeave={() => setIsActive(false)}
+      className={isActive ? "hovered" : ""}
+    >
       <h3>
         {props.tittel}
         <span>{props.pris}</span>
