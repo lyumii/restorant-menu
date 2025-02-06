c7e3b854676eccdb06c389007954ae1daefa2b9d
lyumii <dmly1124@gmail.com>
2025-02-06 19:25:32 +0100

    added hover: with tailwind

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card(props) {
   return (
-    <article className="flex min-w-90 flex-1 gap-4 flex-col flex-wrap justify-center bg-white shadow-md w-1/4 m-5 rounded-md">
+    <article className="flex min-w-90 flex-1 gap-4 flex-col flex-wrap justify-center bg-white shadow-md w-1/4 m-5 rounded-md hover:shadow-xl hover:scale-110">
       <h3 className="bg-cyan-100 text-lg rounded-md p-3 flex justify-between font-semibold text-cyan-950">
         {props.tittel}
         <span className="font-bold">{props.pris}</span>
